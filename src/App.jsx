5d4b1e205dedcce165085120a8a599beed4e0612
Bastien Rouviere <bastien_r@orange.fr>
2025-01-09 14:55:24 +0100

    single coin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SingleCoin from './components/SingleCoin'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <SingleCoin cryptoId={"bitcoin"} />
+    <Router>
+      <Routes>
+        
+        <Route path="/crypto/:cryptoId" element={<CryptoDetailWrapper />} />
+      </Routes>
+    </Router>
     </>
   )
 }
+
+const CryptoDetailWrapper = () => {
+  const { cryptoId } = useParams();
+  return <SingleCoin cryptoId={cryptoId} />;
+};
 
 export default App
